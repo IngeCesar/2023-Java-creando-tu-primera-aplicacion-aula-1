eb0570201c6de0a05fc7c15200107ac297379f4b
IngeCesar <cooh1@ymail.com>
2025-06-02 15:34:06 -0600

    declare la variable clasificacion, que es la mitad de la variable media y la varible clasificacion sera un entero, asi que l avariable media la casteamos a entero tambien, luego hacemos una salida de la variable clasificacion

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,8 @@ public class Main {
                 Fué lanzada en: 
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 2);
+        System.out.println(clasificacion);
     }
 }
